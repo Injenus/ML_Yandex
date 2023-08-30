86a4563f3f685147ddb0cf6949617e31c5a1860b
injenus <injenus777@gmail.com>
2023-08-31 02:33:36 +0300

    Init

---
+++ training_2021/E.py
@@ -0,0 +1,3 @@
+flat_num_1, storeys_num, flat_num_2, entr_num_2, floor_2 = map(int,
+                                                               input().split())
+entr_num_1, floor_1 = None, None
